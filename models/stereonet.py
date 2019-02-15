9c7865b3b4eb49eeac22dfcb30794f820476834c
zhixuanli <zhixuanli520@gmail.com>
2019-02-15 10:15:52 +0800

    fix the bug in issue #4, thanks for @friggog

--- models/stereonet.py
+++ models/stereonet.py
@@ -44,7 +44,6 @@ class StereoNet(nn.Module):
             nn.Conv3d(32, 1, 3, padding=1),
         )
 
-        # TODO what is the input channel size
         self.refine = nn.Sequential(
             nn.Conv2d(4, 32, 3, padding=1),
             ResBlock(32, 32, dilation=1),
@@ -54,16 +53,13 @@ class StereoNet(nn.Module):
             ResBlock(32, 32, dilation=1),
             ResBlock(32, 32, dilation=1),
             nn.Conv2d(32, 1, 3, padding=1),
-            nn.ReLU(),
+            # nn.ReLU(),
         )
 
     def forward_once_1(self, x):
         output = self.downsampling(x)
 
         output = self.res(output)
-
-        # print(x.shape) # torch.Size([4, 3, 375, 1242])
-        # print(output.shape) # torch.Size([4, 32, 18, 72])
 
         return output
 
@@ -76,11 +72,9 @@ class StereoNet(nn.Module):
     def forward_once_2(self, cost_volume):
         """the index cost volume's dimension is not right for conv3d here, so we change it"""
         cost_volume = cost_volume.permute([0, 2, 1, 3, 4])
-        # print("forward_once_2 cost_volume.shape")
-        # print(cost_volume.shape)  # torch.Size([8, 32, 12, 11, 27])
+
         output = self.cost_volume_filter(cost_volume)  # [batch_size, channel, disparity, h, w]
-        # print("output.shape")
-        # print(output.shape)  # torch.Size([8, 1, 10, 9, 25])
+
         disparity_low = soft_argmin(output)
 
         return disparity_low  # low resolution disparity map
@@ -95,59 +89,26 @@ class StereoNet(nn.Module):
 
     def forward_stage3(self, disparity_low, left):
         """upsample and concatenate"""
-        # print(left.shape)
-
-        # d_high_l = nn.functional.upsample_bilinear(disparity_low_l, [left.shape[2], left.shape[3]])
         d_high = nn.functional.interpolate(disparity_low, [left.shape[2], left.shape[3]], mode='bilinear', align_corners=True)
-        # d_high_r = nn.functional.upsample_bilinear(disparity_low_r, [right.shape[2], right.shape[3]])
-        # d_high_r = nn.functional.interpolate(disparity_low_r, [right.shape[2], right.shape[3]])
-
-        # print(disparity_low_l.shape) # torch.Size([4, 1, 16, 70])
-        # print("d_high.shape")  # torch.Size([4, 1, 375, 1242])
-        # print(d_high.shape)  # torch.Size([4, 1, 375, 1242])
-        # print(left.shape)
 
         d_concat = torch.cat([d_high, left], dim=1)
-        # d_concat_r = torch.cat([d_high_r, right], dim=1)
-
-        # print("d_concat.shape")
-        # print(d_concat.shape)  # torch.Size([8, 4, 256, 512])
 
         d_refined = self.refine(d_concat)
-        # d_refined_r = self.forward_once3(d_concat_r)
-
-        # print("d_refined")
-        # print(d_refined.shape)
 
         return d_refined
 
     def forward(self, left, right):
-        # print("left.shape")
-        # print(left.shape)
+
         left_feature, right_feature = self.forward_stage1(left, right)
-        # print("left_feature.shape")
-        # print(left_feature.shape)
-        disparity_low = self.forward_stage2(left_feature, right_feature)
-        # print("left.shape")
-        # print(left.shape)  # torch.Size([8, 3, 256, 512])
-        d_refined = self.forward_stage3(disparity_low, left)
 
-        # print("d_refined.shape")
-        # print(d_refined.shape)  # torch.Size([8, 1, 256, 512])
+        disparity_low_l = self.forward_stage2(left_feature, right_feature)
 
-        # tensor = d_refined_l
-        # unloader = transforms.ToPILImage()
-        # image = tensor.cpu().clone()
-        # image = image.squeeze(1)
-        # image = unloader(image)
-        # image.show()
+        d_initial_l = nn.functional.interpolate(disparity_low_l, [left.shape[2], left.shape[3]], mode='bilinear',
+                                                align_corners=True)
+        d_refined_l = self.forward_stage3(disparity_low_l, left)
+        d_final_l = nn.ReLU()(d_initial_l + d_refined_l)
 
-        # TODO : the paper says here should add them up, check it later
-        # d_final_l = nn.ReLU(disparity_low_r + d_refined_l)
-        # d_final_r = nn.ReLU(disparity_low_r + d_refined_r)
-
-        # return d_final_l, d_final_r
-        return d_refined
+        return d_final_l
 
 class MetricBlock(nn.Module):
     def __init__(self, in_channel, out_channel, stride = 1):
